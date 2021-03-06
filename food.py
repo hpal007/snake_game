ab3822b6531a8e491345f1935e02d976a02b621c
Harishchandra Pal <harishchandra.pal@outlook.com>
2021-03-06 15:36:41 +0530

    gif update

--- food.py
+++ food.py
@@ -16,7 +16,7 @@ class Food(Turtle):
 
     def refresh(self):
         random_x = random.randint(-280, 280)
-        random_y = random.randint(-280, 280)
+        random_y = random.randint(-280, 250)
         self.goto(random_x, random_y)
 
     def color_generator(self):
